08891b49055fb4e4b2f576c3f58704a3cac4e227
rwilt <rosie.wilt@gmail.com>
2020-07-13 11:00:05 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,29 @@
-export function manageFriends(state, action){
+import { defaultConfig } from "sinon"
+
+export function manageFriends(state = {friends: []}, action){
+switch (action.type) {
+    case 'ADD_FRIEND':
+        return ({
+            ...state, friends: [
+                ...state.friends,
+                action.friend
+            ]
+        }
+        )
+    case 'REMOVE_FRIEND':
+        const friendIdx = state.friends.findIndex(friend => friend.id === action.id)
+        return (
+            {...state,
+            friends: [
+                ...state.friends.slice(0, friendIdx),
+                ...state.friends.slice(friendIdx + 1)
+            ]
+        }
+
+        )
+    default: 
+        return state;
+
+}
+
 }
